a2d7cef81f5a1a85915a338c0e51e6f3c967c94b
Sam de Melo <samir.abrahao@yahoo.com>
2026-04-01 15:24:12 -0400

    Changed core_diagram model so it doesn't overwrite certain columns with null values from staging

--- dbanvil/models/core/core_diagram.sql
+++ dbanvil/models/core/core_diagram.sql
@@ -15,6 +15,42 @@
     ]
 ) }}
 
+with source_data as (
+    select
+        diagram_id,
+        owner_id,
+        diagram_name,
+        db_system,
+        legend_enabled,
+        updated_at,
+        load_timestamp
+    from {{ ref('stg_diagram') }}
+)
+
+{% if is_incremental() %}
+
+, existing_data as (
+    select
+        diagram_id,
+        db_system,
+        legend_enabled
+    from {{ this }}
+)
+
+select
+    source_data.diagram_id,
+    source_data.owner_id,
+    source_data.diagram_name,
+    coalesce(source_data.db_system, existing_data.db_system) as db_system,
+    coalesce(source_data.legend_enabled, existing_data.legend_enabled) as legend_enabled,
+    source_data.updated_at,
+    source_data.load_timestamp
+from source_data
+left join existing_data
+    on source_data.diagram_id = existing_data.diagram_id
+
+{% else %}
+
 select
     diagram_id,
     owner_id,
@@ -23,4 +59,6 @@ select
     legend_enabled,
     updated_at,
     load_timestamp
-from {{ ref('stg_diagram') }}
+from source_data
+
+{% endif %}
